15e56bd735c98cc91b1774f33dc0a8b8093e7f8e
Carlos Delfino Carvalho Pinheiro <consultoria@carlosdelfino.eti.br>
2010-11-11 23:58:07 -0300

    more translate.

--- include/client/login.inc.php
+++ include/client/login.inc.php
@@ -12,17 +12,15 @@ $t=Format::input($_POST['lticket']?$_POST['lticket']:$_GET['t']);
     <?}?>
 </div>
 <div style="margin:5px 0px 100px 0;text-align:center; width:100%;">
-    <p align="center">
-        To view the status of a ticket, provide us with your login details below.<br/>
-        If this is your first time contacting us or you've lost the ticket ID, please <a href="open.php">click here</a> to open a new ticket.
+    <p align="center"><?php echo $trl->translate('TEXT_LOGIN_VIEW_STATUS_OF_TICKET');?>;
     </p>
     <span class="error"><?=Format::htmlchars($loginmsg)?></span>
     <form action="login.php" method="post">
     <table cellspacing="1" cellpadding="5" border="0" bgcolor="#000000" align="center">
         <tr bgcolor="#EEEEEE"> 
-            <td>E-Mail:</td><td><input type="text" name="lemail" size="25" value="<?=$e?>"></td>
-            <td>Ticket ID:</td><td><input type="text" name="lticket" size="10" value="<?=$t?>"></td>
-            <td><input class="button" type="submit" value="View Status"></td>
+            <td><?php echo $trl->translate('LABEL_EMAIL'); ?>:</td><td><input type="text" name="lemail" size="25" value="<?=$e?>"></td>
+            <td><?php echo $trl->translate('LABEL_TICKETID')?>:</td><td><input type="text" name="lticket" size="10" value="<?=$t?>"></td>
+            <td><input class="button" type="submit" value="<?php echo $trl->translate('LABEL_VIEW_STATUS');?>"></td>
         </tr>
     </table>
     </form>
