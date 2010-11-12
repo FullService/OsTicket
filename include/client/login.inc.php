daa9bff588ed33d90f5d171a2380a7d94ba15080
Carlos Delfino Carvalho Pinheiro <consultoria@carlosdelfino.eti.br>
2010-11-12 18:50:58 -0300

    correct the translated text.

--- include/client/login.inc.php
+++ include/client/login.inc.php
@@ -19,7 +19,7 @@ $t=Format::input($_POST['lticket']?$_POST['lticket']:$_GET['t']);
     <table cellspacing="1" cellpadding="5" border="0" bgcolor="#000000" align="center">
         <tr bgcolor="#EEEEEE"> 
             <td><?php echo $trl->translate('LABEL_EMAIL'); ?>:</td><td><input type="text" name="lemail" size="25" value="<?=$e?>"></td>
-            <td><?php echo $trl->translate('LABEL_TICKETID')?>:</td><td><input type="text" name="lticket" size="10" value="<?=$t?>"></td>
+            <td><?php echo $trl->translate('LABEL_TICKET_ID')?>:</td><td><input type="text" name="lticket" size="10" value="<?=$t?>"></td>
             <td><input class="button" type="submit" value="<?php echo $trl->translate('LABEL_VIEW_STATUS');?>"></td>
         </tr>
     </table>
