5041541a49244f843409d15adbb32efebb60808c
Carlos Delfino Carvalho Pinheiro <consultoria@carlosdelfino.eti.br>
2010-11-13 05:23:20 +0800

    New struture to use with templates.

---
+++ templates/csat_template/site/client/footer.inc.php
@@ -0,0 +1,9 @@
+ <div style="clear:both"></div> 
+ </div>
+ <div id="footer">Copyright &copy; osTicket.com. All rights reserved</div>
+ <div align="center">
+    <!--> As a show of support, we ask that you leave powered by osTicket link to help spread the word. Thank you! -->
+     <a id="powered_by" href="http://osticket.com"><img src="./images/poweredby.jpg" width="126" height="23" alt="Powered by osTicket"></a></div>
+</div>
+</body>
+</html>
