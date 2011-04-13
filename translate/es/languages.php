98609660f4ebc63d794fb0e5d1b1db450006984f
the0Mummy <millennium_xv@yahoo.com.ar>
2011-04-14 04:00:32 +0800

    Modificado "Espaniol" x "Español"

--- translate/es/languages.php
+++ translate/es/languages.php
@@ -3,7 +3,7 @@
 #Disable direct access.
 if (!strcasecmp(basename($_SERVER['SCRIPT_NAME']), basename( __FILE__ ))) die ('kwaheri rafiki!');
 
-$LANG['ES_NAME'] = 'Espanish';
+$LANG['ES_NAME'] = 'Espa&ntilde;ol';
 $LANG['PT_BR_NAME'] = 'Portugues Brasil';
 $LANG['US_NAME'] = 'English';
 $LANG['NL_NAME'] = 'Dutch';
