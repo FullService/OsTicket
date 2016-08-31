12b0b8291fe1f5591246e1372048e15172e65ce4
Carlos Delfino <consultoria@carlosdelfino.eti.br>
2016-08-31 16:14:12 -0300

    adjusted the support site for my site.
    adjusted the support site for my site.
    Each fork should do it if you want to support directly.

--- setup/upgrade.php
+++ setup/upgrade.php
@@ -37,7 +37,7 @@ define('VERSION','1.6 ST'); //Current version number
 define('SCHEMAFILE','./inc/v16rc5.sql'); //osTicket upgrade SQL schema.
 define('PREFIX',TABLE_PREFIX);
 
-$info='<strong>Need help?</strong> <a href="http://www.osticket.com/support/" target="_blank">Don\'t panic - Professional Upgrade Available!</a>';
+$info='<strong>Need help?</strong> <a href="http://carlosdelfino.eti.br" target="_blank">Don\'t panic - Professional Upgrade Available!</a>';
 //Basic checks 
 $inc='upgrade.inc.php';
 if(!strcasecmp($cfg->getVersion(),VERSION)) {
